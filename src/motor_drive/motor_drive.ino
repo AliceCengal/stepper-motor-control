2ae3e3767a318b42a72b1edc2033613914bbcafb
AliceCengal <cengallut@gmail.com>
2014-05-23 17:17:44 -0500

    Implementing motor control

--- src/motor_drive/motor_drive.ino
+++ src/motor_drive/motor_drive.ino
@@ -1,4 +1,5 @@
 
+#include <math.h>
 #include "MotorCommand.h"
 #include "VirtualMotor.h"
 
@@ -24,19 +25,69 @@ void setup() {
 void loop() {
 }
 
-void doSteps(int pin, int steps, int delayInterval) {
+// A function with continuous 1st derivative
+// in the domain (0,1), with range (0,1)
+double smoothMapping(double x) {
+  return 0.5*(1 - cos(x));
+}
+
+// delayInterval in microseconds
+void doSteps(int pin, int steps, long delayInterval) {
   for (int i = 0; i < steps; ++i) {
     digitalWrite(pin, HIGH);
-    delay(delayInterval);
+    delayMicroseconds(delayInterval);
     digitalWrite(pin, LOW);
-    delay(delayInterval);
+    delayMicroseconds(delayInterval);
   }
 }
+
+// The factor f is choosen such that for a given number
+// of sections n, 
+// 
+//   sum(f * 1/n * sin(pi * ((0.5/n):(1/n):(1-0.5/n)))) == 1.000
+//
+// This is to ensure that the smooth and linear
+// version both have the same duration for
+// a given number of steps.
+const double integralFactor = 1.5643;
+
+double continuousDerivativeAdjustment(double in) {
+  return integralFactor * sin(PI * in);
+}
+
+void doSmoothSteps(int pin, int steps, long delayInterval) {
+  int section = 10;
+  if (steps <= section) {
+    doSteps(pin, steps, delayInterval);
+    return;
+  }
   
+  int sectionSteps = steps / section;
+  int remainder = steps % section;
+  
+  for (int i = 0; i < section; ++i) {
+    if (remainder == 0) {
+      doSteps(pin, sectionSteps, delayInterval * continuousDerivativeAdjustment(double(i)/section));
+    } else {
+      doSteps(pin, sectionSteps + 1, delayInterval * continuousDerivativeAdjustment(double(i)/section));
+      remainder -= 1;
+    }
+  }
+  
+}
+
 void doCommand(CmdPtr c) {
-  int steps = c->steps;
-  int pin = (c->dir == DIR_CW) ? CW_PIN : CCW_PIN;
-  doSteps(pin, steps, (c->motorSpeed)/2);
+  switch(c->dir) {
+    case DIR_STATIONARY:
+      delay(c->steps*1000);
+      break;
+    case DIR_CW:
+      doSteps(CW_PIN, c->steps, 1000000.0/(c->motorSpeed)/2);
+      break;
+    case DIR_CCW:
+      doSteps(CCW_PIN, c->steps, 1000000.0/(c->motorSpeed)/2);
+      break;
+  }
 }
 
 void printSingleCommand(CmdPtr c) {
